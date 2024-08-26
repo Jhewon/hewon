8dc94abb27283cd07308c8ba630c730c12cbe269
EZEN <EZEN@DESKTOP-1O0TJD2>
2024-08-26 14:03:15 +0900

    auth

--- ex00/src/main/java/org/zerock/util/interceptor/AuthrityInterceptor.java
+++ ex00/src/main/java/org/zerock/util/interceptor/AuthrityInterceptor.java
@@ -6,8 +6,10 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.zerock.member.vo.LoginVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -30,6 +32,8 @@ public class AuthrityInterceptor extends HandlerInterceptorAdapter  {
 		authMap.put("/goods/delete.do", 9);
 		authMap.put("/goods/changeImage.do", 9);
 		
+		// 회원 관리
+		authMap.put("/member/logout.do", 1);
 		
 	}
 	
@@ -39,7 +43,22 @@ public class AuthrityInterceptor extends HandlerInterceptorAdapter  {
 		// 권한 처리
 		log.info("권한처리 interceptor ----------------------");
 		
-		// 요청한 내용을 계속 진행
+		// 로그인 했는지 ? - 사용자 등급번호. (login - session) ,  페이지 권한(authMap)
+		// 페이지 권한 가져오기
+		String uri = request.getRequestURI();
+		Integer pageGradeNo = authMap.get(uri);
+		
+		// 로그인이 필요한 경우
+		if(pageGradeNo != null) {
+			HttpSession session = request.getSession();
+			LoginVO loginVO = (LoginVO) session.getAttribute("login");
+			// 로그인이 필요한데 로그인을 안한 경우
+			if(loginVO == null) {
+				return false;
+			}
+		}
+		
+		// 요청한 내용을 계속 진행 - 적합한 권한인 경우
 		return super.preHandle(request, response, handler);
 		} // end of preHandle
 		
