2cd44d8c781a3790052ca7389495aa4ece3737fe
EZEN <EZEN@DESKTOP-1O0TJD2>
2024-08-28 17:04:23 +0900

    category

---
+++ ex00/src/main/java/org/zerock/category/vo/CategoryVO.java
@@ -0,0 +1,13 @@
+package org.zerock.category.vo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class CategoryVO {
+
+	private Integer cate_code1,cate_code2;
+	private String cate_name;
+	
+}
